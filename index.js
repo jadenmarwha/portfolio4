ac6b50f8602c9a61d81b028b3e0a17cda2d3c914
jadenmarwha <143880376+jadenmarwha@users.noreply.github.com>
2023-11-10 11:58:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+<!-- ... Previous HTML ... -->
+
+<section class="moving-text">
+    <h2>I am best at...</h2>
+    <div id="text-container">
+        <div class="text-item">HTML</div>
+        <div class="text-item">CSS</div>
+        <div class="text-item">JavaScript</div>
+        <div class="text-item">React</div>
+        <!-- Add more skills here -->
+    </div>
+    <button id="shuffle-button">Shuffle Skills</button>
+</section>
+
+<!-- ... Previous HTML ... -->
